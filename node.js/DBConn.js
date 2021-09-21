f32f78610aee886e2b14dbd54f10c6f1588b1723
Fernando Camblor <dante.camblor@gmail.com>
2021-09-21 05:24:43 -0400

    started working on the mysql connector for mysql in node.js

---
+++ node.js/DBConn.js
@@ -0,0 +1,69 @@
+
+class MySql {
+    constructor(host, username, password, database) {
+        this.host = host;
+        this.username = username;
+        this.password = password;
+        this.database = database;
+        this.mysql = require('mysql');
+    }
+
+    // init the connection
+    init() {
+        try {
+            this.con = this.mysql.createConnection({
+                host: this.host,
+                user: this.username,
+                password: this.password,
+                database: this.database
+            });
+        } catch (error) {
+            console.log(`${error}`);
+        }
+    }
+
+    // connect to the db
+    connect() {
+        this.con.connect(function (err) {
+            if (err) throw err;
+            console.log("Connected!");
+        });
+    }
+
+    // run the sql query
+    query(query) {
+        this.con.query(query, function (err, result) {
+            if (err) throw err;
+            console.log("Result: " + result);
+        });
+    }
+
+    // inserting many at once
+    insert_many(table_name, rows = [], values = []) {
+        var sql = `INSERT INTO ${table_name} (${rows}) VALUES ?`;
+        // var values = [];
+
+        this.con.query(sql, [values], function (err, result) {
+            if (err) throw err;
+            console.log("Number of records inserted: " + result.affectedRows);
+        });
+    }
+
+    // select all
+    select_all(table_name) {
+        this.con.query(`SELECT * FROM ${table_name}`, function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+        });
+    }
+
+    // select from specific row and table
+    specific_select(table_name, rows = []) {
+        this.con.query(`SELECT (${rows}) FROM ${table_name}`, function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+        });
+    }
+
+}
+
---
+++ node.js/README.txt
@@ -0,0 +1,3 @@
+Run ( npm install ) once you've cd into this file via console to install all the dependencies.
+
+have fun :-) 
